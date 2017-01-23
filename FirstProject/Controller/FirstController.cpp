5d515b654a16d4534f1029e24564b1a62393d074
themoffat28 <bthemoffat@gmail.com>
2017-01-23 09:38:16 -0700

    Made runner and controller class.

--- FirstProject/Controller/FirstController.cpp
+++ FirstProject/Controller/FirstController.cpp
@@ -1,16 +1,17 @@
 //
-//  FirstRunner.cpp
+//  FirstController.cpp
 //  FirstProject
 //
 //  Created by Moffat, Braeden on 1/23/17.
 //  Copyright © 2017 Moffat, Braeden. All rights reserved.
 //
 
+#include <iostream>
 #include "FirstController.h"
 
-int main()
+using namespace std;
+
+void FirstController :: start()
 {
-    FirstController * app = new FirstController();
-    app->start();
-    return 0;
+    cout << "words go here" << endl;
 }
---
+++ FirstProject/Controller/FirstController.h
@@ -0,0 +1,17 @@
+//
+//  FirstController.h
+//  FirstProject
+//
+//  Created by Moffat, Braeden on 1/23/17.
+//  Copyright © 2017 Moffat, Braeden. All rights reserved.
+//
+
+#ifndef FirstController_h
+#define FirstController_h
+
+class FirstController
+{
+public:
+    void start();
+};
+#endif /* FirstController_h */
